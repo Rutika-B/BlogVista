2b4e0940a46ee0da3ff9f61b5ef429f9077e94a4
Rutika Bhuimbar <89460805+Rutika-B@users.noreply.github.com>
2023-11-27 23:24:21 +0530

    Update auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -40,7 +40,7 @@ export class AuthService {
   async getCurrentUser()
   {
     try {
-        return await this.account.get();
+        return await this.account.getCurrentUser();
     } catch (error) {
         console.log("appwrite:: getCurrentUser",error);
     }
